0d50c376cf0f8ec818a9a7d66c92f34b385e014c
chall3ofc-cpu <chall3.ofc@gmail.com>
2026-08-29 20:52:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 99b736e (Första uppladdningen med alla filer)
